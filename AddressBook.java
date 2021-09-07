709034409eda346ab90e6b569323ef653fa048c8
sushant2931 <sushantlad8888@gmail.com>
2021-09-07 18:58:47 +0530

    Add New Contact In Address Book

--- AddressBook.java
+++ AddressBook.java
@@ -1,22 +1,49 @@
 package bl.com.addressbookday10;
+
 import java.util.*;
-public class AddressBook {
-	 String firstName, lastName, address, city, state,email;
-	    long phoneNo;
-	    int zip;
-	    public void setDetails(){
 
-	        firstName = "Sushant";
-	        lastName = "Lad";
-	        address = "BalajiNagar";
-	        city ="Pune";
-	        state = "Maharashta";
-	        zip = 411043;
-			phoneNo = 8830602356;
-	        email = "sushantlad8888@gmail.com";
-	    }
-	    public void printDetails(){
+public class AddressBook extends Contact {
+	String address, city, state;
+	int zip;
+	long phoneNo;
 
-	        System.out.println("\nFirst Nmae : " + firstName  + "\n\nLast Name : " + lastName + "\n\nAddress : " + address + "\n\nCity : " + city + "\n\nState : " + state + "\n\nZip : " + zip + "\n\nPhone Number : " + phoneNo + "\n\nE-mail : " + email); 
-	    }
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public int getZip() {
+		return zip;
+	}
 }
