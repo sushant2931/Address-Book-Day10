aa7c11a2fbfad61e25f76cb9217d5840adff563f
sushant2931 <sushantlad8888@gmail.com>
2021-09-08 03:12:26 +0530

    Using Hashmap

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -5,71 +5,70 @@ import java.util.*;
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome In Address Book System Program On Master Branch");
-		  String firstName, lastName, address, city, state, email;
-	        int zip;
-	        long phoneNo;
-	        char option;
+		String firstName, lastName, address, city, state, email;
+        int zip;
+        long phoneNo;
+        char option;
 
-	        ArrayList<AddressBook> contactArr = new ArrayList<AddressBook>();
+        OperateContact operate = new OperateContact();
+        Scanner sc= new Scanner(System.in);
+        do{
+        
+            AddressBook contact = new AddressBook();
+            System.out.print("\nEnter First Name  : ");  
+            firstName = sc.nextLine();
+            contact.setFirstName(firstName);
 
-	        Scanner sc= new Scanner(System.in);
-	        do{
-	        
-	            AddressBook contact = new AddressBook();
-	            System.out.print("\nEnter First Name  : ");  
-	            firstName = sc.nextLine();
-	            contact.setFirstName(firstName);
+            System.out.print("\nEnter Last Name  : ");  
+            lastName = sc.nextLine();
+            contact.setLastName(lastName);
 
-	            System.out.print("\nEnter Last Name  : ");  
-	            lastName = sc.nextLine();
-	            contact.setLastName(lastName);
+            System.out.print("\nEnter E-mail  : ");
+            email = sc.nextLine();
+            contact.setEmail(email);
 
-	            System.out.print("\nEnter E-mail  : ");
-	            email = sc.nextLine();
-	            contact.setEmail(email);
+            System.out.print("\nEnter Address  : ");  
+            address = sc.nextLine(); 
+            contact.setAddress(address);
 
-	            System.out.print("\nEnter Address  : ");  
-	            address = sc.nextLine(); 
-	            contact.setAddress(address);
+            System.out.print("\nEnter City  : ");  
+            city = sc.nextLine(); 
+            contact.setCity(city);
 
-	            System.out.print("\nEnter City  : ");  
-	            city = sc.nextLine(); 
-	            contact.setCity(city);
+            System.out.print("\nEnter State  : ");  
+            state = sc.nextLine();
+            contact.setState(state);
 
-	            System.out.print("\nEnter State  : ");  
-	            state = sc.nextLine();
-	            contact.setState(state);
+            System.out.print("\nEnter Phone Number  : ");   
+            phoneNo = sc.nextLong();
+            contact.setPhoneNo(phoneNo);
 
-	            System.out.print("\nEnter Phone Number  : ");   
-	            phoneNo = sc.nextLong();
-	            contact.setPhoneNo(phoneNo);
+            System.out.print("\nEnter Zip  : ");  
+            zip = sc.nextInt(); 
+            contact.setZip(zip);
 
-	            System.out.print("\nEnter Zip  : ");  
-	            zip = sc.nextInt(); 
-	            contact.setZip(zip);
+            operate.storeContact(firstName, contact);
 
-	            contactArr.add(contact);
-	            System.out.print("\nDo you want to add one more contact? press Y / N : ");
-	            option = sc.next().charAt(0);
-	            sc.nextLine();
-	        }while(option == 'Y');
+            System.out.print("\nDo you want to add one more contact? press Y / N : ");
+            option = sc.next().charAt(0);
+            sc.nextLine();
+        }while(option == 'Y');
 
-	        OperateContact operate = new OperateContact();
-	        System.out.print("\nDo you want to edit? press Y / N : ");
-	        char editOption = sc.next().charAt(0);
-	        if(editOption == 'Y'){
-	            operate.editContact(contactArr);
-	        }
-	        operate.showContact(contactArr);
-	    
-	        System.out.print("\n\nDo you want to delete? press Y / N : ");
-	        char deleteOption = sc.next().charAt(0);
-	        if(deleteOption == 'Y'){
-	            contactArr = operate.deleteContact(contactArr);
-	        }
-	        operate.showContact(contactArr);
+        System.out.print("\nDo you want to edit? press Y / N : ");
+        char editOption = sc.next().charAt(0);
+        if(editOption == 'Y'){
+            operate.editContact();
+        }
+        operate.showContact();
+    
+        System.out.print("\nDo you want to delete? press Y / N : ");
+        char deleteOption = sc.next().charAt(0);
+        if(deleteOption == 'Y'){
+            operate.deleteContact();
+        }
+        operate.showContact();
 
-	        sc.close();
-	    }
+        sc.close();
+    }
 
 }
