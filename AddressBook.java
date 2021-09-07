a6bfb063eae59fc158b08ea3e617d5537421e5ae
sushant2931 <sushantlad8888@gmail.com>
2021-09-07 18:11:29 +0530

    Create Contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,5 +1,22 @@
 package bl.com.addressbookday10;
-
+import java.util.*;
 public class AddressBook {
+	 String firstName, lastName, address, city, state,email;
+	    long phoneNo;
+	    int zip;
+	    public void setDetails(){
 
+	        firstName = "Sushant";
+	        lastName = "Lad";
+	        address = "BalajiNagar";
+	        city ="Pune";
+	        state = "Maharashta";
+	        zip = 411043;
+			phoneNo = 8830602356;
+	        email = "sushantlad8888@gmail.com";
+	    }
+	    public void printDetails(){
+
+	        System.out.println("\nFirst Nmae : " + firstName  + "\n\nLast Name : " + lastName + "\n\nAddress : " + address + "\n\nCity : " + city + "\n\nState : " + state + "\n\nZip : " + zip + "\n\nPhone Number : " + phoneNo + "\n\nE-mail : " + email); 
+	    }
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,5 +3,9 @@ package bl.com.addressbookday10;
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome In Address Book System Program On Master Branch");
-	}
+		AddressBook address = new AddressBook();
+        address.setDetails();
+        address.printDetails();
+    }
+	
 }
